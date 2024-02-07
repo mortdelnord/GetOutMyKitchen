55163f9f595edcc4a9d22b657b6505d09791c89b
Elijah Rivas <elijah.a.rivas@gmail.com>
2024-02-07 14:16:12 -0500

    brief set up
    set up a scene for testing purposes

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent playerAgent;
+
+    [Header ("Camera")]
+    private Camera mainCam;
+
+    [Header ("Bools")]
+    
+    private bool canMove = true;
+    private bool canInteract = false;
+
+
+    private GameObject currentCarriedObject;
+
+    private void Start()
+    {
+        playerAgent = gameObject.GetComponent<NavMeshAgent>();
+        mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if( Input.GetMouseButton(0))
+        {
+            Ray clickRay = mainCam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(clickRay, out RaycastHit hit, 1000, playerAgent.areaMask))
+            {
+                playerAgent.SetDestination(hit.point);
+
+            }
+        }
+    }
+}
